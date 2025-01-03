0d3e86aea594a736f68f41440173ae5be723e858
GMkonan <guilhemont@hotmail.com>
2025-01-03 19:01:19 -0300

    Fix ts stuff

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,4 +1,4 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { cors } from "@elysiajs/cors";
 import { GetFeeds, AddFeed, GetFeedsWithNotifOn } from "./handlers/feeds";
 import cron from "@elysiajs/cron";
@@ -16,19 +16,92 @@ const app = new Elysia()
 	.use(cors())
 	.use(swagger())
 	.get("/api", () => "Hello Elysia")
-	.post("/new-feed", async (req) => {
-		// should check first in db so we don't add the same feed twice
+	.post(
+		"/new-feed",
+		async (req) => {
+			// should check first in db so we don't add the same feed twice
 
-		const data = await fetchRss(req.body.url);
-		AddFeed(data.feed);
-		// Initial fetch of current posts to enter db
-		AddPosts(data);
-		return { status: "success", feed: data.feed };
-	})
+			const data = await fetchRss(req.body.url);
+			AddFeed(data.feed);
+			// Initial fetch of current posts to enter db
+			AddPosts(data);
+			return { status: "success", feed: data.feed };
+		},
+		{ body: t.Object({ url: t.String() }) },
+	)
+
 	.get("/feeds", async () => {
 		console.log("Running");
 		return await GetFeeds();
 	})
+	.use(
+		cron({
+			name: "update_posts",
+			pattern: "* */2 * * *", // every 2 hours
+			async run() {
+				console.log("Running fetcher cron");
+				// check if there are new posts on all feeds and update accordinly
+
+				const feeds = await GetFeeds();
+
+				for (const feed of feeds) {
+					// fetch rss post
+					const lastRssPost = await fetchRss(feed.rss);
+
+					const lastDbPost = await GetLastPost(feed.id);
+					console.log("Last DB post: ", lastDbPost.pubDate);
+					console.log("Last RSS post: ", lastRssPost.items[0].pubDate);
+					if (
+						new Date(lastRssPost.items[0].pubDate).getTime() >
+						new Date(lastDbPost.pubDate).getTime()
+					) {
+						// add to db
+						AddPosts(lastRssPost);
+					}
+				}
+			},
+		}),
+	)
+	.use(
+		cron({
+			name: "new_posts_mailer",
+			pattern: "0 0 * * *", // every day
+			async run() {
+				console.log("Running");
+
+				// get list of feeds
+				const feeds = await GetFeedsWithNotifOn();
+
+				const postsToSend = [];
+				for (const feed of feeds) {
+					// fetch rss post
+
+					// support more than one post in the future
+					const lastDbPost = await GetLastPost(feed.id);
+
+					// check if post was published after the feed was added to the db
+					if (lastDbPost.pubDate > feed.createdAt) {
+						// check if it's on the list of sent before
+						if (await IsPostOnSentPosts(lastDbPost.id)) {
+							return;
+						}
+
+						// if not, add to list of sent before
+						await MarkPostAsSent(lastDbPost.id);
+						// add to list of send emails
+
+						// maybe add info of author as well
+						postsToSend.push(lastDbPost);
+						console.log(lastDbPost.pubDate);
+						console.log(feed.createdAt);
+					}
+				}
+
+				//send emails
+				await sendEmail(postsToSend);
+			},
+		}),
+	)
 	.listen(3000);
 
 console.log(
