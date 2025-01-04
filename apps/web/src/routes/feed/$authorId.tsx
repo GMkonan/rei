6c7ff949bccd541894a4b6cd02db775a146432af
GMkonan <guilhemont@hotmail.com>
2025-01-04 17:31:31 -0300

    Initial feeds author page structure

--- apps/web/src/routes/feed/$authorId.tsx
+++ apps/web/src/routes/feed/$authorId.tsx
@@ -6,5 +6,21 @@ export const Route = createFileRoute("/feed/$authorId")({
 
 function RouteComponent() {
   const { authorId } = Route.useParams();
-  return <div>{authorId}</div>;
+  // list of posts page
+  // what path should be the actual post path here?
+  return (
+    <div>
+      <h1>Posts by author {authorId}</h1>
+      <span>URL to the original website</span>
+      <div className="flex flex-col bg-slate-300">
+        <h2>Name of the post</h2>
+        <p>Small description of the post</p>
+        <div>
+          <span>Date it was published</span>
+          <span>tags?</span>
+          <span>Path to the original blog post</span>
+        </div>
+      </div>
+    </div>
+  );
 }
